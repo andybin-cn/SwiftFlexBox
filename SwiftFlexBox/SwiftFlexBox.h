a51c8548ef393b603d82d6763f72d9b399db9f09
andy.bin <andy.bin@wolaidai.com>
2019-05-17 10:22:50 +0800

    Initial Commit

---
+++ SwiftFlexBox/SwiftFlexBox.h
@@ -0,0 +1,19 @@
+//
+//  SwiftFlexBox.h
+//  SwiftFlexBox
+//
+//  Created by andy.bin on 2019/5/17.
+//  Copyright © 2019 Binea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftFlexBox.
+FOUNDATION_EXPORT double SwiftFlexBoxVersionNumber;
+
+//! Project version string for SwiftFlexBox.
+FOUNDATION_EXPORT const unsigned char SwiftFlexBoxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftFlexBox/PublicHeader.h>
+
+
